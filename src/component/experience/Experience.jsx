8ca8a425d1643c82f0f444003dadda888edf02e9
admin <mishaplxx@gmail.com>
2022-03-09 22:10:36 +0300

    fix: add about

--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import "./experience.css";
 const Experience = () => {
-  return <section id="experience">experience</section>;
+  return <section id="experience">
+    <h5>What Skills I Have</h5>
+    <h2>My Experience</h2>
+    <div className="container experience__container">
+      <div className="experience__frontend">wqe</div>
+
+    </div>
+  </section>;
 };
 
 export default Experience;
